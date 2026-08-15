f9abb4cac62724c634322f3c1f6eb03ecbd284c4
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-15 19:54:22 +0300

    SC-P9-010 expose customers destination screen

--- mobile/lib/features/navigation/app_shell.dart
+++ mobile/lib/features/navigation/app_shell.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../config/mobile_config.dart';
+import '../customers/customers.dart';
+import '../customers/customers_screen.dart';
 import '../dashboard/dashboard_controller.dart';
 import '../dashboard/dashboard_screen.dart';
 import '../export/mobile_excel_export.dart';
@@ -14,6 +16,7 @@ final class SafeContractsShell extends StatefulWidget {
     required this.config,
     required this.policy,
     required this.dashboardController,
+    required this.customersController,
     required this.excelExportController,
     required this.usingConfigDefaults,
     required this.onClearSession,
@@ -24,6 +27,7 @@ final class SafeContractsShell extends StatefulWidget {
   final SafeContractsMobileConfig config;
   final MobileNavigationPolicy policy;
   final DashboardController dashboardController;
+  final CustomersController customersController;
   final MobileExcelExportController excelExportController;
   final bool usingConfigDefaults;
   final VoidCallback onClearSession;
@@ -94,6 +98,9 @@ final class _SafeContractsShellState extends State<SafeContractsShell> {
     return switch (_selected) {
       MobileDestination.dashboard => DashboardScreen(
           controller: widget.dashboardController,
+        ),
+      MobileDestination.customers => CustomersScreen(
+          controller: widget.customersController,
         ),
       MobileDestination.export => MobileExcelExportScreen(
           controller: widget.excelExportController,
