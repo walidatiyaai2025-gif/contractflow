ade3dc741f46ada5b37c7301d3a5f28f14d69f0f
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-16 01:30:27 +0300

    Load SafeContracts wp-admin branding

--- wordpress-theme/safecontracts-onepage/functions.php
+++ wordpress-theme/safecontracts-onepage/functions.php
@@ -12,6 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once get_theme_file_path( '/inc/i18n.php' );
 require_once get_theme_file_path( '/inc/admin-settings.php' );
 require_once get_theme_file_path( '/inc/button-links.php' );
+require_once get_theme_file_path( '/inc/admin-branding.php' );
 
 /**
  * Register theme capabilities.
