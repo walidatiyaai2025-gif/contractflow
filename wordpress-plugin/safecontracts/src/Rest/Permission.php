25f7717227aa34b9cdc6de2f9a2d5d1436c2f933
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-15 18:41:02 +0300

    P8: add reusable capability permission guard

---
+++ wordpress-plugin/safecontracts/src/Rest/Permission.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SafeContracts\Rest;
+
+use SafeContracts\Roles\AccessScope;
+use WP_Error;
+
+final class Permission
+{
+    public static function access(): bool|WP_Error
+    {
+        if (get_current_user_id() > 0 && AccessScope::canAccess()) {
+            return true;
+        }
+
+        return ApiResponse::error(
+            'safecontracts_forbidden',
+            __('You do not have access to SafeContracts.', 'safecontracts'),
+            403
+        );
+    }
+
+    public static function capability(string $capability, string $code = 'safecontracts_forbidden'): bool|WP_Error
+    {
+        $access = self::access();
+        if ($access instanceof WP_Error) {
+            return $access;
+        }
+        if (current_user_can($capability)) {
+            return true;
+        }
+
+        return ApiResponse::error(
+            $code,
+            __('You do not have permission to perform this SafeContracts operation.', 'safecontracts'),
+            403
+        );
+    }
+}
