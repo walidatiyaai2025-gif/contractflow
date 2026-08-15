d4099c562ef799a6c45dbe2e5451e02d8bd34ba1
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-15 12:54:57 +0300

    feat(contracts): register contract history recorder SC-P2-012

--- wordpress-plugin/safecontracts/src/Plugin.php
+++ wordpress-plugin/safecontracts/src/Plugin.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace SafeContracts;
 
 use SafeContracts\Admin\PaymentMethodsPage;
+use SafeContracts\Contracts\ContractHistoryRecorder;
 use SafeContracts\Database\Migrator;
 use SafeContracts\Rest\Router;
 
@@ -34,6 +35,7 @@ final class Plugin
         // role defaults that administrators may intentionally customize later.
         (new Migrator())->maybeMigrate();
 
+        ContractHistoryRecorder::register();
         add_action('rest_api_init', [Router::class, 'register']);
         add_action('admin_menu', [PaymentMethodsPage::class, 'register']);
         add_action('admin_post_' . PaymentMethodsPage::SAVE_ACTION, [PaymentMethodsPage::class, 'handleSave']);
