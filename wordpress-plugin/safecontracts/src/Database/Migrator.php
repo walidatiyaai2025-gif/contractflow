39b4a27de4b893569184097e99832d0ce9457a33
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-16 23:10:39 +0300

    ESC-P3-005 register org-unit membership migration 1.23.0

--- wordpress-plugin/safecontracts/src/Database/Migrator.php
+++ wordpress-plugin/safecontracts/src/Database/Migrator.php
@@ -28,11 +28,12 @@ use SafeContracts\Database\Migrations\Migration0020EnterpriseParties;
 use SafeContracts\Database\Migrations\Migration0021EnterprisePartyRoles;
 use SafeContracts\Database\Migrations\Migration0022EnterprisePartyRelationships;
 use SafeContracts\Database\Migrations\Migration0023EnterpriseOrgUnits;
+use SafeContracts\Database\Migrations\Migration0024EnterpriseOrgUnitMemberships;
 
 final class Migrator
 {
     public const VERSION_OPTION = 'safecontracts_db_version';
-    public const LATEST_VERSION = '1.22.0';
+    public const LATEST_VERSION = '1.23.0';
 
     /** @var array<string, class-string<Migration>> */
     private const MIGRATIONS = [
@@ -59,6 +60,7 @@ final class Migrator
         '1.20.0' => Migration0021EnterprisePartyRoles::class,
         '1.21.0' => Migration0022EnterprisePartyRelationships::class,
         '1.22.0' => Migration0023EnterpriseOrgUnits::class,
+        '1.23.0' => Migration0024EnterpriseOrgUnitMemberships::class,
     ];
 
     public function maybeMigrate(): void
