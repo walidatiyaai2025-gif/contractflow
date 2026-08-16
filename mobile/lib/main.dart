a5b023771d29330283e4fabe31b1c92f4db4c899
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-16 16:11:45 +0300

    MOBILE: initialize foreground notification presenter

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter/widgets.dart';
 
 import 'app.dart';
 import 'core/config/app_environment.dart';
+import 'features/notifications/notification_presenter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await MobileNotificationPresenter.start();
   runApp(SafeContractsApp(environment: AppEnvironment.fromCompileTime()));
 }
