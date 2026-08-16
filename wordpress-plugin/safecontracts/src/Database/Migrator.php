e97b394df53f99066983d0bc9af760fb201a391e
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-16 23:18:53 +0300

    ESC-P3-006 register Customer Party bridge migration 1.24.0

--- wordpress-plugin/safecontracts/src/Database/Migrator.php
+++ wordpress-plugin/safecontracts/src/Database/Migrator.php
@@ -29,11 +29,12 @@ use SafeContracts\Database\Migrations\Migration0021EnterprisePartyRoles;
 use SafeContracts\Database\Migrations\Migration0022EnterprisePartyRelationships;
 use SafeContracts\Database\Migrations\Migration0023EnterpriseOrgUnits;
 use SafeContracts\Database\Migrations\Migration0024EnterpriseOrgUnitMemberships;
+use SafeContracts\Database\Migrations\Migration0025EnterpriseCustomerPartyLinks;
 
 final class Migrator
 {
     public const VERSION_OPTION = 'safecontracts_db_version';
-    public const LATEST_VERSION = '1.23.0';
+    public const LATEST_VERSION = '1.24.0';
 
     /** @var array<string, class-string<Migration>> */
     private const MIGRATIONS = [
@@ -61,6 +62,7 @@ final class Migrator
         '1.21.0' => Migration0022EnterprisePartyRelationships::class,
         '1.22.0' => Migration0023EnterpriseOrgUnits::class,
         '1.23.0' => Migration0024EnterpriseOrgUnitMemberships::class,
+        '1.24.0' => Migration0025EnterpriseCustomerPartyLinks::class,
     ];
 
     public function maybeMigrate(): void
