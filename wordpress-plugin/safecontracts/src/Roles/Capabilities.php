63275780f1023d7e853cc3586b635a0555bd5df5
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-16 21:39:15 +0300

    Add fine-grained mobile create edit capabilities

--- wordpress-plugin/safecontracts/src/Roles/Capabilities.php
+++ wordpress-plugin/safecontracts/src/Roles/Capabilities.php
@@ -12,9 +12,13 @@ final class Capabilities
     public const MANAGE_USERS = 'safecontracts_manage_users';
     public const VIEW_ALL = 'safecontracts_view_all';
     public const VIEW_ASSIGNED = 'safecontracts_view_assigned';
+    public const CREATE_CUSTOMERS = 'safecontracts_create_customers';
+    public const EDIT_CUSTOMERS = 'safecontracts_edit_customers';
     public const CREATE_CONTRACTS = 'safecontracts_create_contracts';
     public const EDIT_CONTRACTS = 'safecontracts_edit_contracts';
     public const ASSIGN_CONTRACTS = 'safecontracts_assign_contracts';
+    public const CREATE_PAYMENTS = 'safecontracts_create_payments';
+    public const EDIT_PAYMENTS = 'safecontracts_edit_payments';
     public const MANAGE_PAYMENTS = 'safecontracts_manage_payments';
     public const MANAGE_COLLECTIONS = 'safecontracts_manage_collections';
     public const MANAGE_FOLLOWUPS = 'safecontracts_manage_followups';
@@ -34,9 +38,13 @@ final class Capabilities
             self::MANAGE_USERS,
             self::VIEW_ALL,
             self::VIEW_ASSIGNED,
+            self::CREATE_CUSTOMERS,
+            self::EDIT_CUSTOMERS,
             self::CREATE_CONTRACTS,
             self::EDIT_CONTRACTS,
             self::ASSIGN_CONTRACTS,
+            self::CREATE_PAYMENTS,
+            self::EDIT_PAYMENTS,
             self::MANAGE_PAYMENTS,
             self::MANAGE_COLLECTIONS,
             self::MANAGE_FOLLOWUPS,
