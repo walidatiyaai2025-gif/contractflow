0656cc6572a43d9e0346b6c5a478240192bb0e90
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-16 13:17:26 +0300

    Load Safe Contracts theme gettext normalizer

--- wordpress-theme/safecontracts-onepage/functions.php
+++ wordpress-theme/safecontracts-onepage/functions.php
@@ -10,6 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 require_once get_theme_file_path( '/inc/brand.php' );
+require_once get_theme_file_path( '/inc/brand-text.php' );
 require_once get_theme_file_path( '/inc/i18n.php' );
 require_once get_theme_file_path( '/inc/admin-settings.php' );
 require_once get_theme_file_path( '/inc/translation-overrides.php' );
