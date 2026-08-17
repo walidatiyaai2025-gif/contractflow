a33eaf439f2685f2431714398f41686945a54194
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-17 16:25:03 +0300

    ESC-P9-003 register schema version 1.47.0

--- wordpress-plugin/safecontracts/src/Database/Migrator.php
+++ wordpress-plugin/safecontracts/src/Database/Migrator.php
@@ -52,11 +52,12 @@ use SafeContracts\Database\Migrations\Migration0044EnterpriseContractMilestones;
 use SafeContracts\Database\Migrations\Migration0045EnterpriseContractRenewalTerms;
 use SafeContracts\Database\Migrations\Migration0046EnterpriseContractNoticePeriodRules;
 use SafeContracts\Database\Migrations\Migration0047EnterpriseContractDeliverables;
+use SafeContracts\Database\Migrations\Migration0048EnterpriseContractFinancialCurrencyProfiles;
 
 final class Migrator
 {
     public const VERSION_OPTION = 'safecontracts_db_version';
-    public const LATEST_VERSION = '1.46.0';
+    public const LATEST_VERSION = '1.47.0';
 
     /** @var array<string, class-string<Migration>> */
     private const MIGRATIONS = [
@@ -107,6 +108,7 @@ final class Migrator
         '1.44.0' => Migration0045EnterpriseContractRenewalTerms::class,
         '1.45.0' => Migration0046EnterpriseContractNoticePeriodRules::class,
         '1.46.0' => Migration0047EnterpriseContractDeliverables::class,
+        '1.47.0' => Migration0048EnterpriseContractFinancialCurrencyProfiles::class,
     ];
 
     public function maybeMigrate(): void
