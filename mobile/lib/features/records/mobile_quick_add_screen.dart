135f3018b8acc7dfa50447d1fd8709d8ac566d1e
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-18 00:32:07 +0300

    SC-P11-004 delegate quick-add screen to canonical flow

--- mobile/lib/features/records/mobile_quick_add_screen.dart
+++ mobile/lib/features/records/mobile_quick_add_screen.dart
@@ -1,5 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../../core/api/api_client.dart';
+import '../session/session_controller.dart';
 import 'mobile_quick_add_flow.dart' as flow;
 
 export 'mobile_quick_add_flow.dart' hide MobileQuickAddScreen;
 
-typedef MobileQuickAddScreen = flow.MobileQuickAddScreen;
+final class MobileQuickAddScreen extends StatelessWidget {
+  const MobileQuickAddScreen({
+    required this.client,
+    required this.session,
+    required this.type,
+    super.key,
+  });
+
+  final SafeContractsApiClient client;
+  final SafeContractsSession session;
+  final flow.MobileQuickAddType type;
+
+  @override
+  Widget build(BuildContext context) {
+    return flow.MobileQuickAddScreen(
+      client: client,
+      session: session,
+      type: type,
+    );
+  }
+}
