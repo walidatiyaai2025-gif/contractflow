f890e1bee8ec72d69b743d0d65cc3333d72b2855
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-23 12:36:04 +0300

    Advance schema for multi-entity attachments

--- wordpress-plugin/safecontracts/src/Database/Migrator.php
+++ wordpress-plugin/safecontracts/src/Database/Migrator.php
@@ -24,13 +24,14 @@ use SafeContracts\Database\Migrations\Migration0017CounterpartySupplierApar;
 use SafeContracts\Database\Migrations\Migration0018SupplierFinanceReconciliation;
 use SafeContracts\Database\Migrations\Migration0019NullableLegacyCustomer;
 use SafeContracts\Database\Migrations\Migration0020NotificationRecipientRepair;
+use SafeContracts\Database\Migrations\Migration0021EntityAttachments;
 use RuntimeException;
 use Throwable;
 
 final class Migrator
 {
     public const VERSION_OPTION = 'safecontracts_db_version';
-    public const LATEST_VERSION = '1.19.0';
+    public const LATEST_VERSION = '1.21.0';
 
     /**
      * All migrations introduced after this already-released baseline must use
@@ -60,6 +61,7 @@ final class Migrator
         '1.17.0' => Migration0018SupplierFinanceReconciliation::class,
         '1.18.0' => Migration0019NullableLegacyCustomer::class,
         '1.19.0' => Migration0020NotificationRecipientRepair::class,
+        '1.21.0' => Migration0021EntityAttachments::class,
     ];
 
     /** @var array<string, class-string<Migration>> */
