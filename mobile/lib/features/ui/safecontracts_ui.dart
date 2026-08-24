61eee998d93404f54ed2b44134dfd311d86dac8d
walidatiyaai2025-gif <walidatiyaai2025@gmail.com>
2026-08-24 10:43:18 +0300

    feat(mobile-ui): expose shared Alkenzy UI toolkit

---
+++ mobile/lib/features/ui/safecontracts_ui.dart
@@ -0,0 +1,7 @@
+export 'mobile_layout.dart';
+export 'safecontracts_components.dart';
+export 'safecontracts_design.dart';
+export 'safecontracts_form.dart';
+export 'safecontracts_splash.dart';
+export 'safecontracts_theme.dart';
+export 'safecontracts_tokens.dart';
